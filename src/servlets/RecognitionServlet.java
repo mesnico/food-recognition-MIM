d4aa8f66cbda08e3f5210c6f14b6acc95e2856bd
m.gasperini <marco.gasperini93@libero.it>
2017-06-09 13:57:38 +0200

    Class RecognitionServlet created. The POST request sent by the form is assigned to it which is able to response. For now the response only consists in a SUCCESS PAGE but later it will perform the food recognition and will answer with the classification and the most similar images

---
+++ src/servlets/RecognitionServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by utente on 09/06/2017.
+ */
+
+public class RecognitionServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer=response.getWriter();
+        writer.write("<!DOCTYPE html>"+
+                            "<html lang='en'> <head> <meta charset='UTF-8'>"+
+                            "<title>Success Creation</title> </head>"+
+                            "<body> <h2>L'azione &eacute stata completata con successo!</h2>"+
+                            "<a href='../index.html'>Clicca qui per tornare al pannello principale</a>"+
+                            "</body> </html>");
+        response.setStatus(404);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
