7ce9fbe81e2aefe929f1c11f3762ee61996b6066
Indrit <indritkertusha91@gmail.com>
2017-06-09 18:02:00 +0200

    commit test

--- src/it/unipi/ing/mim/deep/DNNExtractor.java
+++ src/it/unipi/ing/mim/deep/DNNExtractor.java
@@ -24,6 +24,7 @@ public class DNNExtractor {
 	
 	public DNNExtractor() {		
 		//Create the importer of Caffe framework network
+		//HAHAHAHHAHAHAH
 		Importer importer = createCaffeImporter(new File(Parameters.DEEP_PROTO).getPath(), new File(Parameters.DEEP_MODEL).getPath());
 		
 		//Initialize the network
