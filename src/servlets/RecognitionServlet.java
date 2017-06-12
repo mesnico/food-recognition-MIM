c870d13bba6ad3596cbf14b99b3100a16358d1b9
m.gasperini <marco.gasperini93@libero.it>
2017-06-12 11:54:39 +0200

    Servlet works!!

--- src/servlets/RecognitionServlet.java
+++ src/servlets/RecognitionServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +11,6 @@ import java.io.PrintWriter;
 /**
  * Created by utente on 09/06/2017.
  */
-
 public class RecognitionServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter writer=response.getWriter();
@@ -18,9 +18,9 @@ public class RecognitionServlet extends HttpServlet {
                             "<html lang='en'> <head> <meta charset='UTF-8'>"+
                             "<title>Success Creation</title> </head>"+
                             "<body> <h2>L'azione &eacute stata completata con successo!</h2>"+
-                            "<a href='../index.html'>Clicca qui per tornare al pannello principale</a>"+
+                            "<a href='index.html'>Clicca qui per tornare al pannello principale</a>"+
                             "</body> </html>");
-        response.setStatus(404);
+        response.setStatus(200);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
